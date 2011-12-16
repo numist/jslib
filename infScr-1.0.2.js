c4696fc15b4f3ebbe21edf35aad014dd0f3dcab9
Scott Perry <dev@numist.net>
2011-12-16 00:40:39 -0800

    slightly cleaner encapsulation by λ

--- infScr-1.0.2.js
+++ infScr-1.0.2.js
@@ -42,147 +42,150 @@
  * • detectHistorySupport() from Modernizr
  *   released under the MIT License.
  *   http://www.modernizr.com/download/
+ *
+ * Minification by http://www.refresh-sf.com/yui/
  */
 
 // loading feedback used when XHR is active
 var infScrLoadingFeedback = '<p>loading…</p>';
 
 /*****************************************************************************/
-
-// infScrStates = states of the InfScr system:
-var infScrStates = {
-  idle: 0,    // (-> loading)
-  loading: 1, // (-> success | idle)
-  success: 2  // page updated with new posts (-> idle)
-};
-
-// infScr = current state of infinite scroll
-var infScrState = infScrStates.idle;
-
-// first page in range of pages shown, set once.
-var infScrUrlBasePage = null;
-
-// worker function
-function infScrExecute() {
-  // updates browser history to reflect the new range of pages being displayed
-  function updatepath(path) {
-    if(infScrUrlBasePage == null) {
-      // we need a base page number
-      regexp = /\/pages?\/([0-9]+)-?[0-9]*\/?$/;
-      
-      if(regexp.test(window.location.href)) {
-        // base page in current location
-        nextPage = regexp.exec(window.location.href);
-        infScrUrlBasePage = nextPage[1];
-      } else {
-        // use the base page number of the next page (minus one)
-        nextPage = regexp.exec(path);
-        infScrUrlBasePage = nextPage[1] - 1;
+(function(){
+  // infScrStates = states of the InfScr system:
+  var infScrStates = {
+    idle: 0,    // (-> loading)
+    loading: 1, // (-> success | idle)
+    success: 2  // page updated with new posts (-> idle)
+  };
+  
+  // infScr = current state of infinite scroll
+  var infScrState = infScrStates.idle;
+  
+  // first page in range of pages shown, set once.
+  var infScrUrlBasePage = null;
+  
+  // worker function
+  function infScrExecute() {
+    // updates browser history to reflect the new range of pages being displayed
+    function updatepath(path) {
+      if(infScrUrlBasePage == null) {
+        // we need a base page number
+        regexp = /\/pages?\/([0-9]+)-?[0-9]*\/?$/;
+        
+        if(regexp.test(window.location.href)) {
+          // base page in current location
+          nextPage = regexp.exec(window.location.href);
+          infScrUrlBasePage = nextPage[1];
+        } else {
+          // use the base page number of the next page (minus one)
+          nextPage = regexp.exec(path);
+          infScrUrlBasePage = nextPage[1] - 1;
+        }
       }
+      
+      // create link with range from the base page number to the next page number
+      path = path.replace("page/","pages/"+infScrUrlBasePage+"-");
+      
+      // NOTE: replaceState only allows same-origin changes, see:
+      // https://developer.mozilla.org/en/DOM/Manipulating_the_browser_history#The_pushState().C2.A0method
+      window.history.replaceState(null, null, path);
     }
     
-    // create link with range from the base page number to the next page number
-    path = path.replace("page/","pages/"+infScrUrlBasePage+"-");
-    
-    // NOTE: replaceState only allows same-origin changes, see:
-    // https://developer.mozilla.org/en/DOM/Manipulating_the_browser_history#The_pushState().C2.A0method
-    window.history.replaceState(null, null, path);
+    function scriptmatch(script, haystack) {
+      for(i = 0; i < haystack.length; i++) {
+        if($(script).html() == $(haystack[i]).html()) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    /*
+     * get more content if:
+     * • not already loading new content
+     * • viewport is less than one $(window).height() from bottom of document.
+     *   see: http://www.tbray.org/ongoing/When/201x/2011/11/26/Misscrolling
+     */
+    if(infScrState == infScrStates.idle
+    && $(document).height() < $(document).scrollTop() + (2 * $(window).height()))
+    {
+      // block potentially concurrent requests
+      infScrState = infScrStates.loading;
+  
+      // get next page's loading node and URL
+      var moreNode = $('article#more').last();
+      var moreURL = moreNode.find('a').last().attr("href");
+  
+      // make request if node was found, not hidden, and updatepath is supported
+      if(moreURL.length > 0 && moreNode.css('display') != 'none'
+      && !!(window.history && history.replaceState))
+      {
+        $.ajax({
+          type: 'GET',
+          url: moreURL,
+          beforeSend: function() {
+            // display loading feedback
+            moreNode.clone().empty().insertBefore(moreNode).append(infScrLoadingFeedback);
+  
+            // hide 'more' browser
+            moreNode.hide();
+          },
+          success: function(data) {
+            // use nodetype to grab elements
+            var filteredData = $(data).find("article");
+  
+            if(filteredData.length > 0) {
+              // found valid data
+              infScrState = infScrStates.success;
+  
+              // drop data into document
+              filteredData.insertAfter(moreNode);
+  
+              // insert and evaluate scripts unique to the page
+              $(data).filter("script").each(function(index, item) {
+                if(!scriptmatch(item, $(document).find("script"))) {
+                  var element = document.createElement("script");
+          				element.charset = "utf-8";
+          				element.type = "text/javascript";
+                  if(item.src) {
+            			  element.src = item.src;
+                  } else if($(item).html().length) {
+                    element.innerHTML = $(item).html();
+                  }
+          			  document.body.appendChild(element);
+                }
+              });
+  
+              // update the address bar
+              updatepath(moreURL)
+            }
+          },
+          complete: function(jqXHR, textStatus) {
+            // remove loading feedback
+            moreNode.prev().remove();
+  
+            // if our XHR did not add data to the page,
+            if(infScrState != infScrStates.success) {
+              // unhide 'more' browser
+              moreNode.show();
+            }
+  
+            infScrState = infScrStates.idle;
+          },
+          dataType: "html"
+        });
+      }
+    }
   }
   
-  function scriptmatch(script, haystack) {
-    for(i = 0; i < haystack.length; i++) {
-      if($(script).html() == $(haystack[i]).html()) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /*
-   * get more content if:
-   * • not already loading new content
-   * • viewport is less than one $(window).height() from bottom of document.
-   *   see: http://www.tbray.org/ongoing/When/201x/2011/11/26/Misscrolling
-   */
-  if(infScrState == infScrStates.idle
-  && $(document).height() < $(document).scrollTop() + (2 * $(window).height()))
-  {
-    // block potentially concurrent requests
-    infScrState = infScrStates.loading;
-
-    // get next page's loading node and URL
-    moreNode = $('article#more').last();
-    moreURL = moreNode.find('a').last().attr("href");
-
-    // make request if node was found, not hidden, and updatepath is supported
-    if(moreURL.length > 0 && moreNode.css('display') != 'none'
-    && !!(window.history && history.replaceState))
-    {
-      $.ajax({
-        type: 'GET',
-        url: moreURL,
-        beforeSend: function() {
-          // display loading feedback
-          moreNode.clone().empty().insertBefore(moreNode).append(infScrLoadingFeedback);
-
-          // hide 'more' browser
-          moreNode.hide();
-        },
-        success: function(data) {
-          // use nodetype to grab elements
-          var filteredData = $(data).find("article");
-
-          if(filteredData.length > 0) {
-            // found valid data
-            infScrState = infScrStates.success;
-
-            // drop data into document
-            filteredData.insertAfter(moreNode);
-
-            // insert and evaluate scripts unique to the page
-            $(data).filter("script").each(function(index, item) {
-              if(!scriptmatch(item, $(document).find("script"))) {
-                var element = document.createElement("script");
-        				element.charset = "utf-8";
-        				element.type = "text/javascript";
-                if(item.src) {
-          			  element.src = item.src;
-                } else if($(item).html().length) {
-                  element.innerHTML = $(item).html();
-                }
-        			  document.body.appendChild(element);
-              }
-            });
-
-            // update the address bar
-            updatepath(moreURL)
-          }
-        },
-        complete: function(jqXHR, textStatus) {
-          // remove loading feedback
-          moreNode.prev().remove();
-
-          // if our XHR did not add data to the page,
-          if(infScrState != infScrStates.success) {
-            // unhide 'more' browser
-            moreNode.show();
-          }
-
-          infScrState = infScrStates.idle;
-        },
-        dataType: "html"
-      });
-    }
-  }
-}
-
-$(document).ready(function () {
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  // http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
-  $(window).scroll(function() {
-    $.doTimeout( 'scroll', 200 /* milliseconds */, infScrExecute);
+  $(document).ready(function () {
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    // http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+    $(window).scroll(function() {
+      $.doTimeout('scroll', 200 /* milliseconds */, infScrExecute);
+    });
   });
-});
+})();
 
 /*
  * Permission is hereby granted, free of charge, to any person obtaining a copy
